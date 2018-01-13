d9137161cedd82e245ffd772d5d8f9e8ae6d56e6
Jorge Aparicio <jorge@japaric.io>
2018-01-14 00:19:19 +0100

    switch to a stable embedded-hal release

--- ci/install.sh
+++ ci/install.sh
@@ -1,24 +1,34 @@
 set -euxo pipefail
 
 main() {
-    # This fetches latest stable release of Cross
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-                    | cut -d/ -f3 \
-                    | grep -E '^v[0.1.0-9.]+$' \
-                    | sort --version-sort \
-                    | tail -n1)
-    curl -LSfs https://japaric.github.io/trust/install.sh | \
-        sh -s -- \
-           --force \
-           --git japaric/cross \
-           --tag $tag
-
     case $TARGET in
         thumbv*-none-eabi*)
-            cargo install --list | grep xargo || \
-                cargo install xargo
-            rustup component list | grep 'rust-src.*installed' || \
-                rustup component add rust-src
+            # This fetches latest stable release of Xargo
+            local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/xargo \
+                            | cut -d/ -f3 \
+                            | grep -E '^v[0.3.0-9.]+$' \
+                            | sort --version-sort \
+                            | tail -n1)
+
+            curl -LSfs https://japaric.github.io/trust/install.sh | \
+                sh -s -- \
+                   --force \
+                   --git japaric/xargo \
+                   --tag $tag
+            ;;
+        *)
+            # This fetches latest stable release of Cross
+            local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+                            | cut -d/ -f3 \
+                            | grep -E '^v[0.1.0-9.]+$' \
+                            | sort --version-sort \
+                            | tail -n1)
+
+            curl -LSfs https://japaric.github.io/trust/install.sh | \
+                sh -s -- \
+                   --force \
+                   --git japaric/cross \
+                   --tag $tag
             ;;
     esac
 }
--- examples/rpi.rs
+++ examples/rpi.rs
@@ -11,54 +11,16 @@
 //! - PIN22 = BCM25 = NCS
 //! - PIN6 = GND = GND
 
-extern crate embedded_hal as hal;
+extern crate linux_embedded_hal as hal;
 extern crate mpu9250;
-extern crate spidev;
-extern crate sysfs_gpio;
 
-use std::io::{self, Write};
 use std::thread;
 use std::time::Duration;
 
 use mpu9250::Mpu9250;
-use spidev::{Spidev, SpidevOptions, SpidevTransfer};
-use sysfs_gpio::{Direction, Pin};
-
-pub struct MySpidev(Spidev);
-pub struct MyPin(Pin);
-
-impl hal::blocking::spi::FullDuplex<u8> for MySpidev {
-    type Error = io::Error;
-
-    fn transfer<'b>(&mut self, buffer: &'b mut [u8]) -> Result<&'b [u8], io::Error> {
-        let tx = buffer.to_owned();
-        self.0
-            .transfer(&mut SpidevTransfer::read_write(&tx, buffer))?;
-        Ok(buffer)
-    }
-
-    fn write(&mut self, bytes: &[u8]) -> Result<(), io::Error> {
-        self.0.write_all(bytes)
-    }
-}
-
-impl hal::digital::OutputPin for MyPin {
-    fn set_high(&mut self) {
-        self.0.set_value(1).unwrap()
-    }
-
-    fn set_low(&mut self) {
-        self.0.set_value(0).unwrap()
-    }
-
-    fn is_low(&self) -> bool {
-        self.0.get_value().unwrap() == 0
-    }
-
-    fn is_high(&self) -> bool {
-        self.0.get_value().unwrap() == 1
-    }
-}
+use hal::spidev::{self, SpidevOptions};
+use hal::{Delay, Pin, Spidev};
+use hal::sysfs_gpio::Direction;
 
 fn main() {
     let mut spi = Spidev::open("/dev/spidev0.0").unwrap();
@@ -74,7 +36,7 @@ fn main() {
     ncs.set_direction(Direction::Out).unwrap();
     ncs.set_value(1).unwrap();
 
-    let mut mpu9250 = Mpu9250::new(MySpidev(spi), MyPin(ncs)).unwrap();
+    let mut mpu9250 = Mpu9250::new(spi, ncs, &mut Delay).unwrap();
 
     let who_am_i = mpu9250.who_am_i().unwrap();
     let ak8963_who_am_i = mpu9250.ak8963_who_am_i().unwrap();
