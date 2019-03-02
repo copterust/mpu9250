bbb47781983306746434751026729c6a1df5219c
Ian McIntyre <ianpmcintyre@gmail.com>
2019-03-02 15:52:41 -0500

    Update BBBlue example for IMU
    MARG bring-up is not correct when using I2C. Need to investigate
    further...
    
    Rip-off the output of the rc_test_mpu tool that's on the BBBlue.

--- examples/bbblue.rs
+++ examples/bbblue.rs
@@ -6,6 +6,7 @@
 extern crate linux_embedded_hal as hal;
 extern crate mpu9250;
 
+use std::io::{self, Write};
 use std::thread;
 use std::time::Duration;
 
@@ -13,19 +14,31 @@ use hal::Delay;
 use hal::I2cdev;
 use mpu9250::Mpu9250;
 
-fn main() {
+fn main() -> io::Result<()> {
     let i2c = I2cdev::new("/dev/i2c-2").expect("unable to open /dev/i2c-2");
 
-    let mut mpu9250 = Mpu9250::marg_default(i2c, &mut Delay).expect("unable to make MPU9250");
+    let mut mpu9250 =
+        Mpu9250::imu_default(i2c, &mut Delay).expect("unable to make MPU9250");
 
     let who_am_i = mpu9250.who_am_i().expect("could not read WHO_AM_I");
 
     println!("WHO_AM_I: 0x{:x}", who_am_i);
     assert_eq!(who_am_i, 0x71);
 
-    println!("{:#?}", mpu9250.all().expect("could not perform first read all"));
-
-    thread::sleep(Duration::from_millis(100));
-
-    println!("{:#?}", mpu9250.all().unwrap());
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
+    writeln!(&mut stdout, "   Accel XYZ(m/s^2)  |   Gyro XYZ (deg/s)  |")?;
+    loop {
+        let all = mpu9250.all().expect("unable to read from MPU!");
+        write!(&mut stdout,
+               "\r{:>6.2} {:>6.2} {:>6.2} |{:>6.1} {:>6.1} {:>6.1} |",
+               all.accel[0],
+               all.accel[1],
+               all.accel[2],
+               all.gyro[0],
+               all.gyro[1],
+               all.gyro[2])?;
+        stdout.flush()?;
+        thread::sleep(Duration::from_micros(100000));
+    }
 }
