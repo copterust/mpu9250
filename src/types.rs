8bbdb64784d4b031af5fc1f9d1f10f0df13a5cdf
Thomas Chamelot <chamelot.thomas@gmail.com>
2020-03-16 19:25:33 +0100

    Add Dmp MpuMode

--- src/types.rs
+++ src/types.rs
@@ -7,3 +7,7 @@ impl MpuMode for Imu {}
 /// Magnetometer + Accelerometer + Gyroscope + Temperature Sensor
 pub struct Marg;
 impl MpuMode for Marg {}
+
+/// Accelerometer + Gyroscope + Dmp
+pub struct Dmp;
+impl MpuMode for Dmp {}
